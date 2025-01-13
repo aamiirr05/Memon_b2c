5caf548a8755b3339adf5023de34d964758ac48b
aqeebpathan <developer.aqeeb@gmail.com>
2025-01-13 20:04:39 +0530

    Update: forex form

--- Frontend/src/pages/ForexPage.jsx
+++ Frontend/src/pages/ForexPage.jsx
@@ -1,9 +1,10 @@
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import { ArrowLeft } from 'lucide-react';
 
 import { useForexFormStore } from '../store/useForexFormStore';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 // Validation schema
 const schema = yup.object().shape({
@@ -46,16 +47,40 @@ const ForexInquiryForm = () => {
   };
 
   return (
-    <div className="h-screen bg-peach/10 flex justify-center items-center">
-      <div className=" text-darkgreen bg-peach p-8 rounded-xl shadow-lg max-w-3xl">
-        <h2 className="text-center text-3xl font-bold mb-12">Forex Enquiry</h2>
+    <div className="h-screen w-screen bg-darkgreen flex flex-row">
+      <div className="relative flex-1 bg-peach m-4 rounded-xl flex flex-col items-center justify-center">
+        <Link
+          to="/"
+          className="absolute top-8 left-8 text-sm font-medium text-darkgreen bg-darkgreen/20 hover:bg-darkgreen/80 transition-colors hover:text-peach rounded-full flex justify-center items-center gap-1 px-2 py-1"
+        >
+          <ArrowLeft size={18} />
+          <span className="mt-[-2px] mr-1">Back to home</span>
+        </Link>
+        <div className="flex justify-center items-center flex-col">
+          <img src="/forex.png" alt="forex" className="w-48 h-48 mb-12" />
+          <h3 className="font-bold text-xl text-darkgreen">
+            Simlify Your Hajj & Umrah Payments
+          </h3>
+          <p className="max-w-md text-center text-darkgreen tracking-tight leading-tight mt-2">
+            Exchange currencies effortlessly for your Hajj or Umrah journey.
+            Complete the form on the right to get the best forex rates and a
+            seamless payment experience.
+          </p>
+        </div>
+      </div>
+
+      {/* form */}
+      <div className="flex-1 flex flex-col gap-12 justify-center items-center">
+        <h1 className="text-3xl font-semibold text-peach font-jakarta tracking-tight">
+          FOREX FORM
+        </h1>
         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6">
           <div className="grid md:grid-cols-2 gap-4">
             <div id="input" className="relative">
               <select
                 {...register('salutation')}
                 id="salutation"
-                className="block w-full text-sm h-[50px] px-4 text-slate-900 bg-white rounded-[8px] border border-violet-200 appearance-none focus:border-transparent focus:outline focus:outline-2 focus:outline-primary focus:ring-0 hover:border-brand-500-secondary peer invalid:border-error-500 invalid:focus:border-error-500"
+                className="block w-full text-sm h-[50px] px-4 text-peach bg-darkgreen rounded-[8px] border border-peach appearance-none focus:border-transparent focus:outline focus:outline-2 focus:outline-peach focus:ring-0 peer invalid:border-error-500 invalid:focus:border-error-500"
               >
                 <option value="">Select Salutation</option>
                 <option value="Mr">Mr</option>
@@ -64,7 +89,7 @@ const ForexInquiryForm = () => {
               </select>
               <label
                 htmlFor="salutation"
-                className="peer-placeholder-shown:-z-10 peer-focus:z-10 absolute text-[14px] leading-[150%] text-primary peer-focus:text-primary peer-invalid:text-error-500 focus:invalid:text-error-500 duration-300 transform -translate-y-[1.2rem] scale-75 font-medium left-2 top-2 z-10 origin-[0] bg-darkgreen text-peach rounded-full  disabled:bg-gray-50-background- px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-[1.2rem]"
+                className="peer-placeholder-shown:-z-10 peer-focus:z-10 absolute text-[14px] leading-[150%] text-primary peer-focus:text-primary peer-invalid:text-error-500 focus:invalid:text-error-500 duration-300 transform -translate-y-[1.2rem] scale-75 font-medium left-2 top-2 z-10 origin-[0] bg-peach text-darkgreen rounded-full  disabled:bg-gray-50-background- px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-[1.2rem]"
               >
                 Salutation
               </label>
@@ -81,14 +106,11 @@ const ForexInquiryForm = () => {
                 type="text"
                 {...register('firstname')}
                 id="firstname"
-                className="block w-full text-sm h-[50px] px-4 text-slate-900 bg-white rounded-[8px] border border-violet-200 appearance-none focus:border-transparent focus:outline focus:outline-2 focus:outline-darkgreen focus:ring-0 hover:border-brand-500-secondary peer invalid:border-error-500 invalid:focus:border-error-500 overflow-ellipsis overflow-hidden text-nowrap pr-[48px]"
-                placeholder="First name"
+                className="forex-input-fields peer"
+                placeholder="Firstname"
               />
-              <label
-                htmlFor="firstname"
-                className="peer-placeholder-shown:-z-10 peer-focus:z-10 absolute text-[14px] leading-[150%] text-primary peer-focus:text-primary peer-invalid:text-error-500 focus:invalid:text-error-500 duration-300 transform -translate-y-[1.2rem] scale-75 font-medium left-2 top-2 z-10 origin-[0] bg-darkgreen text-peach rounded-full disabled:bg-gray-50-background- px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-[1.2rem]"
-              >
-                First name
+              <label htmlFor="firstname" className="forex-input-labels">
+                Firstname
               </label>
               <p className="text-red-600 text-sm mt-1">
                 {errors.firstname?.message}
@@ -101,14 +123,11 @@ const ForexInquiryForm = () => {
                 type="text"
                 {...register('lastname')}
                 id="lastname"
-                className="block w-full text-sm h-[50px] px-4 text-slate-900 bg-white rounded-[8px] border border-violet-200 appearance-none focus:border-transparent focus:outline focus:outline-2 focus:outline-darkgreen focus:ring-0 hover:border-brand-500-secondary peer invalid:border-error-500 invalid:focus:border-error-500 overflow-ellipsis overflow-hidden text-nowrap pr-[48px]"
-                placeholder="Last name"
+                className="forex-input-fields peer"
+                placeholder="Lastname"
               />
-              <label
-                htmlFor="lastname"
-                className="peer-placeholder-shown:-z-10 peer-focus:z-10 absolute text-[14px] leading-[150%] text-primary peer-focus:text-primary peer-invalid:text-error-500 focus:invalid:text-error-500 duration-300 transform -translate-y-[1.2rem] scale-75 font-medium left-2 top-2 z-10 origin-[0] bg-darkgreen text-peach rounded-full disabled:bg-gray-50-background- px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-[1.2rem]"
-              >
-                Last name
+              <label htmlFor="lastname" className="forex-input-labels">
+                Lastname
               </label>
               <p className="text-red-600 text-sm mt-1">
                 {errors.lastname?.message}
@@ -123,13 +142,10 @@ const ForexInquiryForm = () => {
                 type="email"
                 {...register('email')}
                 id="email"
-                className="block w-full text-sm h-[50px] px-4 text-slate-900 bg-white rounded-[8px] border border-violet-200 appearance-none focus:border-transparent focus:outline focus:outline-2 focus:outline-darkgreen focus:ring-0 hover:border-brand-500-secondary peer invalid:border-error-500 invalid:focus:border-error-500 overflow-ellipsis overflow-hidden text-nowrap pr-[48px]"
+                className="forex-input-fields peer"
                 placeholder="Email"
               />
-              <label
-                htmlFor="email"
-                className="peer-placeholder-shown:-z-10 peer-focus:z-10 absolute text-[14px] leading-[150%] text-primary peer-focus:text-primary peer-invalid:text-error-500 focus:invalid:text-error-500 duration-300 transform -translate-y-[1.2rem] scale-75 font-medium left-2 top-2 z-10 origin-[0] bg-darkgreen text-peach rounded-full disabled:bg-gray-50-background- px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-[1.2rem]"
-              >
+              <label htmlFor="email" className="forex-input-labels">
                 Email
               </label>
               <p className="text-red-600 text-sm mt-1">
@@ -143,13 +159,10 @@ const ForexInquiryForm = () => {
                 type="text"
                 {...register('contact')}
                 id="contact"
-                className="block w-full text-sm h-[50px] px-4 text-slate-900 bg-white rounded-[8px] border border-violet-200 appearance-none focus:border-transparent focus:outline focus:outline-2 focus:outline-darkgreen focus:ring-0 hover:border-brand-500-secondary peer invalid:border-error-500 invalid:focus:border-error-500 overflow-ellipsis overflow-hidden text-nowrap pr-[48px]"
+                className="forex-input-fields peer"
                 placeholder="Contact"
               />
-              <label
-                htmlFor="contact"
-                className="peer-placeholder-shown:-z-10 peer-focus:z-10 absolute text-[14px] leading-[150%] text-primary peer-focus:text-primary peer-invalid:text-error-500 focus:invalid:text-error-500 duration-300 transform -translate-y-[1.2rem] scale-75 font-medium left-2 top-2 z-10 origin-[0] bg-darkgreen text-peach rounded-full disabled:bg-gray-50-background- px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-[1.2rem]"
-              >
+              <label htmlFor="contact" className="forex-input-labels">
                 Contact
               </label>
               <p className="text-red-600 text-sm mt-1">
@@ -162,16 +175,13 @@ const ForexInquiryForm = () => {
             {/* Amount Required */}
             <div id="input" className="relative">
               <input
-                type="number"
+                type="text"
                 {...register('amountrequired')}
                 id="amountrequired"
-                className="block w-full text-sm h-[50px] px-4 text-slate-900 bg-white rounded-[8px] border border-violet-200 appearance-none focus:border-transparent focus:outline focus:outline-2 focus:outline-darkgreen focus:ring-0 hover:border-brand-500-secondary peer invalid:border-error-500 invalid:focus:border-error-500"
+                className="forex-input-fields peer"
                 placeholder="Amount Required"
               />
-              <label
-                htmlFor="amountrequired"
-                className="peer-placeholder-shown:-z-10 peer-focus:z-10 absolute text-[14px] leading-[150%] text-primary peer-focus:text-primary peer-invalid:text-error-500 focus:invalid:text-error-500 duration-300 transform -translate-y-[1.2rem] scale-75 font-medium left-2 top-2 z-10 origin-[0] bg-darkgreen text-peach rounded-full disabled:bg-gray-50-background- px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-[1.2rem]"
-              >
+              <label htmlFor="amountrequired" className="forex-input-labels">
                 Amount Required
               </label>
               <p className="text-red-600 text-sm mt-1">
@@ -185,13 +195,10 @@ const ForexInquiryForm = () => {
                 type="text"
                 {...register('country')}
                 id="country"
-                className="block w-full text-sm h-[50px] px-4 text-slate-900 bg-white rounded-[8px] border border-violet-200 appearance-none focus:border-transparent focus:outline focus:outline-2 focus:outline-darkgreen focus:ring-0 hover:border-brand-500-secondary peer invalid:border-error-500 invalid:focus:border-error-500"
-                placeholder="Enter Country"
+                className="forex-input-fields peer"
+                placeholder="Country"
               />
-              <label
-                htmlFor="country"
-                className="peer-placeholder-shown:-z-10 peer-focus:z-10 absolute text-[14px] leading-[150%] text-primary peer-focus:text-primary peer-invalid:text-error-500 focus:invalid:text-error-500 duration-300 transform -translate-y-[1.2rem] scale-75 font-medium left-2 top-2 z-10 origin-[0] bg-darkgreen text-peach rounded-full disabled:bg-gray-50-background- px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-[1.2rem]"
-              >
+              <label htmlFor="country" className="forex-input-labels">
                 Country
               </label>
               <p className="text-red-600 text-sm mt-1">
@@ -207,13 +214,10 @@ const ForexInquiryForm = () => {
                 type="text"
                 {...register('address')}
                 id="address"
-                className="block w-full text-sm h-[50px] px-4 text-slate-900 bg-white rounded-[8px] border border-violet-200 appearance-none focus:border-transparent focus:outline focus:outline-2 focus:outline-darkgreen focus:ring-0 hover:border-brand-500-secondary peer invalid:border-error-500 invalid:focus:border-error-500 "
-                placeholder="Enter Address"
+                className="forex-input-fields peer"
+                placeholder="Address"
               />
-              <label
-                htmlFor="country"
-                className="peer-placeholder-shown:-z-10 peer-focus:z-10 absolute text-[14px] leading-[150%] text-primary peer-focus:text-primary peer-invalid:text-error-500 focus:invalid:text-error-500 duration-300 transform -translate-y-[1.2rem] scale-75 font-medium left-2 top-2 z-10 origin-[0] bg-darkgreen text-peach rounded-full disabled:bg-gray-50-background- px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-[1.2rem]"
-              >
+              <label htmlFor="country" className="forex-input-labels">
                 Address
               </label>
               <p className="text-red-600 text-sm mt-1">
@@ -223,18 +227,15 @@ const ForexInquiryForm = () => {
           </div>
 
           {/* Submit Button */}
-          <div className="flex justify-center items-center">
-            <button
-              type="submit"
-              className="py-3 px-8 bg-darkgreen text-peach text-sm font-medium rounded-md hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-darkgreen focus:ring-offset-2 w-fit"
-              disabled={isSubmitting}
-            >
-              {isSubmitting ? 'Submitting...' : 'Submit'}
-            </button>
-          </div>
+          <button
+            type="submit"
+            className="w-full py-3 px-8 bg-peach text-darkgreen text-sm font-medium rounded-md hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-darkgreen focus:ring-offset-2"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'Submitting...' : 'Submit'}
+          </button>
         </form>
       </div>
-      {/* <RoomNameInput /> */}
     </div>
   );
 };
