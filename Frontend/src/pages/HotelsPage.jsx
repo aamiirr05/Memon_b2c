96aa77120fe28da74158227a238442fd352e02c7
aqeebpathan <developer.aqeeb@gmail.com>
2025-01-21 16:10:56 +0530

    Add: hotels page

---
+++ Frontend/src/pages/HotelsPage.jsx
@@ -0,0 +1,67 @@
+import { useEffect } from 'react';
+import Filter from '../components/PackagesPage/Filter';
+import { useHotelStore } from '../store/useHotelStore';
+import HotelCard from '../components/HotelsPage.jsx/HotelCard';
+
+const HotelsPage = () => {
+  const { fetchHotels, hotels, areHotelsFetched } = useHotelStore();
+  useEffect(() => {
+    if (!areHotelsFetched) {
+      fetchHotels();
+    }
+  }, [fetchHotels]);
+
+  console.log(hotels);
+
+  return (
+    <main className="bg-peach/50">
+      <section>
+        <div className="max-w-6xl mx-auto px-4 lg:px-0">
+          <div className="pt-12 pb-8">
+            <h2 className="text-center text-4xl text-darkgreen font-zodiak mt-4 mb-4">
+              <span className="hidden md:inline">
+                {' '}
+                Discover Comfort and Convenience:{' '}
+              </span>
+              Our Handpicked Hotels for Umrah & Hajj Pilgrims
+            </h2>
+            <p className="text-center text-lg text-darkgreen mb-6">
+              Stay Near the Holy Sites with Our Premium Hotel Options in Makkah
+              & Madinah
+            </p>
+          </div>
+
+          <div className="pb-14">
+            <div className=" flex gap-6">
+              <Filter />
+
+              <div className="flex flex-col gap-6 w-full">
+                {hotels.map((hotel, index) => (
+                  <HotelCard key={hotel.hotel_id} />
+                ))}
+              </div>
+
+              {/* listing all packages pkg = package; 'package' is a reserved word in strict mode. Modules are automatically in strict mode.*/}
+              {/* <div className="flex flex-col gap-6 w-full">
+                {isFetching ? (
+                  Array(3)
+                    .fill()
+                    .map((_, index) => <PackageCardSkeleton key={index} />)
+                ) : (
+                  <>
+                    {' '}
+                    {packages.map((pkg) => (
+                      <PackageCard key={pkg.package_id} pkg={pkg} />
+                    ))}
+                  </>
+                )}
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default HotelsPage;
---
+++ Frontend/src/store/useHotelStore.js
@@ -0,0 +1,21 @@
+import { create } from 'zustand';
+import axiosInstance from '../lib/axios';
+import toast from 'react-hot-toast';
+
+export const useHotelStore = create((set) => ({
+  hotels: [],
+  isFetching: false,
+  areHotelsFetched: false,
+  fetchHotels: async () => {
+    set({ isFetching: true });
+    try {
+      const res = await axiosInstance.get('/users/fetch-all-hotels');
+      set({ hotels: res.data.data, areHotelsFetched: true });
+    } catch (error) {
+      toast.error(error.response.data.message || 'Error while fetching hotels');
+      console.log(error);
+    } finally {
+      set({ isFetching: false });
+    }
+  },
+}));
