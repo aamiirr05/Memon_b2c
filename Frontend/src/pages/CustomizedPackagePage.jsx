ed35790aebe0b628083c300890e5acfbbbd95411
Taufiqkhan12 <ktaufiq913@gmail.com>
2025-01-21 12:15:29 +0530

    Update Customized Package Form

--- Frontend/src/pages/CustomizedPackagePage.jsx
+++ Frontend/src/pages/CustomizedPackagePage.jsx
@@ -49,7 +49,7 @@ const CustomizedPackagePage = () => {
 
   return (
     <div>
-      <div className="flex-1 flex flex-col gap-12 py-6 px-6 justify-center items-center bg-darkgreen">
+      <div className="flex-1 flex flex-col gap-12 pt-6 pb-20 rounded-md px-6 justify-center items-center bg-darkgreen m-8">
         <h1 className="text-3xl font-semibold text-peach tracking-tight mt-6 ">
           CUSTOMIZED PACKAGE ENQUIRY
         </h1>
