e1b07eeab3c65c80a46d6bbc823aebfd27b6d214
aqeebpathan <developer.aqeeb@gmail.com>
2025-01-26 19:21:12 +0530

    Adjust: border

--- Frontend/src/components/PackageDetailPage/BentoGrid.jsx
+++ Frontend/src/components/PackageDetailPage/BentoGrid.jsx
@@ -12,11 +12,11 @@ const BentoGrid = ({ images }) => {
   return (
     <div className="flex flex-col gap-4 md:flex-row items-start space-x-6 sm:p-4 sm:border-2 border-dotted border-darkgreen rounded-md overflow-clip">
       {/* Large image on the left with fixed size */}
-      <div className="flex-shrink-0 flex-1 w-full h-[300px] md:h-[400px] lg:h-[500px] overflow-hidden sm:border-2 border-darkgreen rounded-xl">
+      <div className="flex-shrink-0 flex-1 w-full h-[300px] md:h-[400px] lg:h-[500px] overflow-hidden sm:border border-darkgreen rounded-md">
         <img
           src={mainImage}
           alt="Main"
-          className="w-full h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] object-cover rounded-xl"
+          className="w-full h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] object-cover rounded-md"
         />
       </div>
 
@@ -27,13 +27,13 @@ const BentoGrid = ({ images }) => {
           .map(({ secure_url }, index) => (
             <div
               key={index}
-              className="cursor-pointer transition-transform transform hover:scale-[1.02]"
+              className="cursor-pointer transition-transform transform hover:scale-[1.01]"
               onClick={() => changeImage(secure_url)}
             >
               <img
                 src={secure_url}
                 alt={`Small ${index + 1}`}
-                className="w-full h-[100px] sm:h-[150px] md:w-[300px] md:h-[192px] lg:w-[340px] lg:h-[240px] object-cover rounded-lg sm:border-2 border-darkgreen"
+                className="w-full h-[100px] sm:h-[150px] md:w-[300px] md:h-[192px] lg:w-[340px] lg:h-[240px] object-cover rounded-md sm:border border-darkgreen"
               />
             </div>
           ))}
