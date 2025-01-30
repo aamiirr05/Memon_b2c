a745a2fac7189b46294b90086a249f0679bac553
aqeebpathan <developer.aqeeb@gmail.com>
2025-01-30 12:19:53 +0530

    Adjust: tab styles

--- Frontend/src/components/HotelDetailPage/Tabs/TermsTab.jsx
+++ Frontend/src/components/HotelDetailPage/Tabs/TermsTab.jsx
@@ -1,15 +1,19 @@
+import { Handshake } from 'lucide-react';
 import { useHotelStore } from '../../../store/useHotelStore';
 
 const TermsTab = () => {
   const { selectedHotel } = useHotelStore();
 
-  if (!selectedHotel) return <p>Loading...</p>;
-
   return (
     <ul>
       {selectedHotel.term_condition?.length ? (
         selectedHotel.term_condition.map((term, index) => (
-          <li key={index}>{term}</li>
+          <li key={index} className="flex items-start gap-4 mb-2">
+            <div className="bg-darkgreen/40 text-darkgreen rounded-full p-1 mt-1">
+              <Handshake size={16} />
+            </div>
+            <span className="text-base text-neutral-700">{term}</span>
+          </li>
         ))
       ) : (
         <li>No terms available</li>
