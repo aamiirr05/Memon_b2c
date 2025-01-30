525bf2365b794d40284be7320b8dcd30ec474fba
aqeebpathan <developer.aqeeb@gmail.com>
2025-01-30 15:47:49 +0530

    ...

--- Frontend/src/components/PackagesPage/Filter.jsx
+++ Frontend/src/components/PackagesPage/Filter.jsx
@@ -1,6 +1,6 @@
 const Filter = () => {
   return (
-    <div className="sticky top-0 bg-darkgreen/10 h-screen w-72 hidden rounded-2xl md:flex">
+    <div className="sticky top-16 bg-darkgreen/10 h-[calc(100dvh-64px)] w-72 hidden rounded-2xl md:flex items-center justify-center text-xl text-neutral-500">
       Filter
     </div>
   );
