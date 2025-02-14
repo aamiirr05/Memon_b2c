a60f315fce33b51303027d1ac016a1a04daaea29
zaidachhwa <zaidachhwa8486@gmail.com>
2025-02-14 17:55:24 +0530

    Landing page completed

--- Frontend/src/components/HomePage/HoildayGrid.jsx
+++ Frontend/src/components/HomePage/HoildayGrid.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/prop-types */
 import { MoveRight, TicketsPlane, TreePalm } from 'lucide-react';
-import { SuitcaseRolling } from '@phosphor-icons/react';
+import {
+  AirplaneLanding,
+  AirplaneTakeoff,
+  SuitcaseRolling,
+} from '@phosphor-icons/react';
 import useFetchPackages from '../../Admin/hooks/UseFetchPackages';
 import { NavLink } from 'react-router-dom';
 import { useInView, motion } from 'framer-motion';
@@ -19,6 +23,8 @@ const HolidayGrid = ({ isMenuOpen }) => {
   const filterCards = getPackages?.data?.data.filter((_, i) => i < 2) || [];
 
   const cards = [...filterCards];
+
+  console.log(cards);
   return (
     <>
       <section
@@ -43,7 +49,7 @@ const HolidayGrid = ({ isMenuOpen }) => {
 
         <div
           ref={ref}
-          className="w-11/12 h-[60vh] lg:w-3/4 mt-40 mb-20 md:mb-10 md:mt-16 mx-auto flex-col md:flex-row flex items-start justify-center gap-3"
+          className="w-11/12 sm:h-[500px] lg:w-3/4 mt-40 mb-20 md:mb-10 md:mt-16 mx-auto flex-col md:flex-row flex items-start justify-center gap-3"
         >
           {/* Grid 1  */}
           <div className="w-full flex flex-col gap-3">
@@ -51,19 +57,40 @@ const HolidayGrid = ({ isMenuOpen }) => {
             <motion.div
               animate={{ x: isInView ? 0 : '10vw', opacity: isInView ? 1 : 0 }}
               transition={{ duration: 2, type: 'spring', delay: 0.1 }}
-              className="w-full flex gap-3 h-[30vw] md:h-[25vh] lg:h-[13vw]"
+              className="w-full flex gap-3 h-[200px]"
             >
-              <NavLink
-                to={`holidays/holiday-details/${cards[0]?.package_id}`}
-                className="w-3/4 rounded-lg overflow-hidden shadow-md cursor-pointer"
-              >
+              <div className="w-full group relative rounded-lg overflow-hidden shadow-md cursor-pointer">
+                <div className="absolute flex flex-col gap-5 transition-all duration-500 group-hover:rounded-t-lg group-hover:backdrop-blur-sm h-10 group-hover: bottom-0 group-hover:h-full bg-peach/90 group-hover:bg-peach/70 w-full p-3 font-jakarta text-darkgreen font-semibold">
+                  <div className="flex items-center justify-between">
+                    <div className="text-xs">{cards[0]?.country}</div>
+                    <div className="text-xs sm:text-sm flex items-center">
+                      <span className="hidden lg:flex">Starting from :</span> ₹
+                      {cards[0]?.final_price}
+                    </div>
+                  </div>
+                  <div className="text-xs sm:text-sm">{`${cards[0]?.total_days} Days / ${cards[0]?.total_nights} Nights `}</div>
+                  <div className="flex items-center justify-between">
+                    <div className="text-xs sm:text-smflex items-center gap-2">
+                      <AirplaneTakeoff size={20} /> {cards[0]?.departure_city}
+                    </div>
+                    <div className="text-xs sm:text-sm flex items-center gap-2 md:mr-20">
+                      <AirplaneLanding size={20} /> {cards[0]?.arrival_city}
+                    </div>
+                  </div>
+                  <NavLink
+                    to={`holidays/holiday-details/${cards[0]?.package_id}`}
+                    className="w-fit px-3 sm:px-6 bg-darkgreen rounded-md p-2 text-xs sm:text-sm font-zodiak text-peach"
+                  >
+                    View Details
+                  </NavLink>
+                </div>
                 <img
                   src={cards[0]?.package_images[0]?.secure_url}
                   alt=""
-                  className="w-full h-full hover:opacity-60"
+                  className="w-full h-full object-cover"
                 />
-              </NavLink>
-              <div className="w-full shadow-md bg-darkgreen text-peach rounded-lg flex gap-3 flex-col items-end justify-center p-2 md:p-5">
+              </div>
+              <div className="w-3/4 hidden shadow-md bg-darkgreen text-peach rounded-lg md:flex gap-3 flex-col items-end justify-center p-2 md:p-5">
                 <h1 className="font-zodiak font-semibold text-lg italic">
                   <TreePalm size={60} />
                 </h1>
@@ -76,19 +103,40 @@ const HolidayGrid = ({ isMenuOpen }) => {
             <motion.div
               animate={{ x: isInView ? 0 : '-10vw', opacity: isInView ? 1 : 0 }}
               transition={{ duration: 2, type: 'spring', delay: 0.1 }}
-              className="w-full flex flex-row-reverse gap-3 h-[30vw] md:h-[24vh] lg:h-[12vw]"
+              className="w-full flex flex-row-reverse gap-3 h-[200px]"
             >
-              <NavLink
-                to={`holidays/holiday-details/${cards[1]?.package_id}`}
-                className="w-3/4 rounded-lg overflow-hidden shadow-md cursor-pointer"
-              >
+              <div className="w-full relative group rounded-lg overflow-hidden shadow-md cursor-pointer">
+                <div className="absolute flex flex-col gap-5 transition-all duration-500 group-hover:rounded-t-lg group-hover:backdrop-blur-sm h-10 group-hover: bottom-0 group-hover:h-full bg-darkgreen/90 group-hover:bg-darkgreen/70 w-full p-3 font-jakarta text-peach font-semibold">
+                  <div className="flex items-center justify-between">
+                    <div className="text-xs">{cards[1]?.country}</div>
+                    <div className="text-xs sm:text-sm flex items-center">
+                      <span className="hidden lg:flex">Starting from :</span> ₹
+                      {cards[1]?.final_price}
+                    </div>
+                  </div>
+                  <div className="text-xs sm:text-sm">{`${cards[0]?.total_days} Days / ${cards[0]?.total_nights} Nights `}</div>
+                  <div className="flex items-center justify-between">
+                    <div className="text-xs sm:text-sm flex items-center gap-2">
+                      <AirplaneTakeoff size={20} /> {cards[1]?.departure_city}
+                    </div>
+                    <div className="text-xs sm:text-sm flex items-center gap-2 mr-20">
+                      <AirplaneLanding size={20} /> {cards[1]?.arrival_city}
+                    </div>
+                  </div>
+                  <NavLink
+                    to={`holidays/holiday-details/${cards[1]?.package_id}`}
+                    className="w-fit px-3 sm:px-6 bg-peach/80 rounded-md p-2 text-xs sm:text-sm font-zodiak text-darkgreen"
+                  >
+                    View Details
+                  </NavLink>
+                </div>
                 <img
                   src={cards[1]?.package_images[0]?.secure_url}
                   alt=""
-                  className="w-full h-full hover:opacity-60"
+                  className="w-full h-full object-cover"
                 />
-              </NavLink>
-              <div className="w-full shadow-md bg-peach text-darkgreen rounded-lg flex gap-3 flex-col items-start justify-center p-2 md:p-5">
+              </div>
+              <div className="w-3/4 hidden md:flex shadow-md bg-peach text-darkgreen rounded-lg  gap-3 flex-col items-start justify-center p-2 md:p-5">
                 <h1 className="font-zodiak font-semibold italic">
                   <TicketsPlane size={60} />
                 </h1>
@@ -102,7 +150,7 @@ const HolidayGrid = ({ isMenuOpen }) => {
             <motion.div
               animate={{ y: isInView ? 0 : '10vw', opacity: isInView ? 1 : 0 }}
               transition={{ duration: 2, type: 'spring', delay: 0.1 }}
-              className="w-full h-[7.3vh] shadow-md rounded-lg bg-peach/70 flex items-center justify-center font-jakarta font-semibold text-xs lg:text-sm text-darkgreen p-2"
+              className="w-full h-[76px] shadow-md rounded-lg bg-peach/70 flex items-center justify-center font-jakarta font-semibold text-xs lg:text-sm text-darkgreen p-2"
             >
               “Book now and enjoy up to 30% off select holiday packages!”
             </motion.div>
